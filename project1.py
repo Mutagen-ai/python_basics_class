c7596828f75195c0a5b79b5f7ac840eb6cadee32
Mutagen-ai <ingangabob@gmail.com>
2026-03-21 22:05:16 +0300

    Initial commit

---
+++ project1.py
@@ -0,0 +1,28 @@
+"""print("Welcome to python")
+
+name = input("Enter your name:")
+score = int(input("Enter your mean score:"))
+if score >90 and score <= 100:
+    print(f"{name} scored an A")
+elif score >80 and score <= 90:
+    print(f"{name} scored an A-")
+elif score >70 and score <= 80:
+    print(f"{name} scored a B+")
+elif score >60 and score <= 70:
+    print(f"{name} scored a B")
+elif score >50 and score <= 60:
+    print(f"{name} scored a C+")
+elif score >40 and score <= 50:
+    print(f"{name} scored a C")
+elif score >10 and score <=40:
+    print(f"{name} scored a pass")
+else:
+    print("Invalid grade")
+"""
+    
+    
+
+        
+python_class = ["Harriet", "Tony", "Bob", "Boniface", "Robrert", "Wenci", "Elizabeth", "Elvis"]
+for x in python_class:
+    print(x)
